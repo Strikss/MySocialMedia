6abaf266afbff6b8c65e559c50c9e277261e5dbf
Mikhail Voloshyn <mikhailvoloshyn@gmail.com>
2021-09-08 18:47:16 +0300

    added containers

---
+++ Components/Dialogs/DialogsContainer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { addSmsActionCreator } from "../../Redux/dialogReducer";
+import { changeSmsActionCreater } from "../../Redux/dialogReducer";
+import Dialogs from "./Dialogs";
+
+const DialogsContainer = (props) => {
+let state = props.store.getState();
+  let onSmsChange = (text) => {
+    props.store.dispatch(changeSmsActionCreater(text));
+  };
+  let addSms = () => {
+    props.store.dispatch(addSmsActionCreator());
+  };
+  return (
+    <Dialogs 
+    updateSmsText={onSmsChange}
+    addSms={addSms}
+    newMessageText={state.messagesPage.newMessageText}
+    dialog={state.messagesPage}
+    />
+  );
+};
+export default DialogsContainer;
+
